bec541f79c0357007ebc35920985f59eae7ec97f
Adam Cmiel <acmiel@redhat.com>
2021-02-25 15:18:20 +0100

    Add a real dependency
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/chmeliik/gomod-test-local-deps
 
 go 1.15
 
-require github.com/chmeliik/some-module v0.0.0
+require (
+	github.com/chmeliik/some-module v0.0.0
+	rsc.io/quote v1.5.2
+)
 
 replace github.com/chmeliik/some-module => ./example/src/some-module
--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/chmeliik/some-module"
+	"rsc.io/quote"
 )
 
 
 func main() {
 	fmt.Println("Hello, local dependencies.")
 	some_module.Hi()
+	fmt.Println("And now for some pithy sayings...\n")
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Opt())
 }
